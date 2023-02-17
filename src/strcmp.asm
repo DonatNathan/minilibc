67ff1ddc44ad68db03da17e6d5e57f1701988c0e
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-02-17 12:31:08 +0100

    [ADD] - Strcmp pretty good

--- src/strcmp.asm
+++ src/strcmp.asm
@@ -3,7 +3,29 @@ section .bss
 section .text
 global strcmp
 
-;strcmp:
-;	ret
+strcmp:
+	xor rcx, rcx ;Set cmpt to 0
+
+.loop:
+    cmp byte [rdi + rcx], 0
+    je .stop
+    mov al, [rsi + rcx]
+    cmp byte [rdi + rcx], al
+    jl .stop_less
+    jg .stop_greater
+    inc rcx ;Inc cmpt
+    jmp .loop
+
+.stop_less:
+    mov rax, -1
+    ret
+
+.stop_greater:
+    mov rax, 1
+    ret
+
+.stop:
+    mov rax, 0
+    ret
 
 section .data
