61356342bd8d861debac2971ab5e5fdefbb6b45a
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-02-18 15:24:37 +0100

    [ADD] - Strcspn

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -4,6 +4,32 @@ section .text
 global strcspn
 
 strcspn:
+	xor rcx, rcx
+	xor rdx, rdx
+
+.loop:
+	cmp byte [rdi], 0
+	je .return
+	mov al, [rsi + rcx]
+	cmp al, 0
+	je .reset
+	cmp al, [rdi]
+	je .got
+	inc rcx
+	jmp .loop
+
+.reset:
+	xor rcx, rcx
+	inc rdi
+	inc rdx
+	jmp .loop
+
+.got:
+	mov rax, rdx
+	ret
+
+.return:
+	mov rax, rdx
 	ret
 
 section .data
