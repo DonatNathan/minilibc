530c9ed5c2776f50179fa2de71497e89e095476b
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-02-18 14:52:04 +0100

    [ADD] - Strstr

--- src/strstr.asm
+++ src/strstr.asm
@@ -4,6 +4,41 @@ section .text
 global strstr
 
 strstr:
+	xor rcx, rcx
+
+.loop:
+	cmp byte [rdi], 0
+	je .return
+	mov al, [rsi]
+	cmp byte [rdi], al
+	je .first
+	inc rdi
+	jmp .loop
+
+.compare:
+	mov al, [rsi + rcx]
+	cmp al, 0
+	je .got
+	cmp byte [rdi], al
+	jne .failed
+	inc rcx
+	inc rdi
+	jmp .compare
+
+.first:
+	mov rdx, rdi
+	jmp .compare
+
+.failed:
+	xor rcx, rcx
+	jmp .loop
+
+.got:
+	mov rax, rdx
+	ret
+
+.return:
+	xor rax, rax
 	ret
 
 section .data
