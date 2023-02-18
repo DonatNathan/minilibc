845cb14784582a069093b3d1ff5bdfe415287ed4
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-02-18 15:06:47 +0100

    [ADD] - Strpbrk

--- src/strpbrk.asm
+++ src/strpbrk.asm
@@ -4,6 +4,31 @@ section .text
 global strpbrk
 
 strpbrk:
+	xor rcx, rcx
+
+.loop:
+	cmp byte [rdi], 0
+	je .return
+	mov al, [rsi + rcx]
+	cmp al, 0
+	je .reset
+	cmp al, [rdi]
+	je .got
+	inc rcx
+	jmp .loop
+
+.reset:
+	xor rcx, rcx
+	inc rdi
+	jmp .loop
+
+
+.got:
+	mov rax, rdi
+	ret
+
+.return:
+	xor rax, rax
 	ret
 
 section .data
