76b426a9cfef8ae9290d8f31723c12a2f69c5dc1
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-02-18 12:01:15 +0100

    [ADD] - Strcasecmp

--- src/strcasecmp.asm
+++ src/strcasecmp.asm
@@ -4,6 +4,32 @@ section .text
 global strcasecmp
 
 strcasecmp:
-	ret
+	xor rcx, rcx ;Set cmpt to 0
+
+.loop:
+    mov al, [rsi + rcx]
+    cmp byte [rdi + rcx], al
+    jl .stop_less
+    jg .stop_greater
+    cmp byte [rdi + rcx], 0
+    je .stop
+    cmp al, 0
+    je .stop_less
+    inc rcx ;Inc cmpt
+    jmp .loop
+
+.stop_less:
+    mov rax, -1
+    ret
+
+.stop_greater:
+    mov rax, 1
+    ret
+
+.stop:
+    cmp al, 0
+    jne .stop_less
+    mov rax, 0
+    ret
 
 section .data
